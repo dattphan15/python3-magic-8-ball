1a5bb6a098e15af3ad836c1906fd1b02032ce583
Kevin Phan <ken.phan@live.ca>
2021-09-25 13:32:42 -0700

    import random module to use .randint() function

--- magic8.py
+++ magic8.py
@@ -1,3 +1,11 @@
+import random
+
 name = "Kevin"
 question = "Is Python3 the latest Python version?"
 answer = ""
+
+# generate a random number between 1 (inclusive) and 9 (inclusive).
+random_number = random.randint(1, 9)
+
+# prints a random number
+print(random_number)
